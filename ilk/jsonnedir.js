18abff913272c94a10a5f673bd84f8903917e773
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-05-02 14:32:45 +0300

    klasör silindi

--- ilk/jsonnedir.js
+++
@@ -1,9 +0,0 @@
-/*
-----------------------JSON Nedir?--------------------------------
--->Projede hangi paketlerin ve hangi versiyonlarının kullanılacağını json dosyasından oluşturulur
--->Proje ile ilgili genel bilgileri verir.
--->Aynı zamanda dependency de verir.
---------------------Express Nedir ?---------------------------------
--->Node.js ile oluşturulan web uygulamalarını daha kolay oluşturmamızı sağlar.
--->Belli yönlendirmeler ve işlevsellikler yapar
-*/
